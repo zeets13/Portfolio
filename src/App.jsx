13d5e9277824b921b38898f2c1eecfd578258bb9
zeets13 <jarifatasnim13@gmail.com>
2026-07-15 23:52:55 +0600

    About and Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import about from "./assets/about.jpg";
 function App() {
   return (
     <>
+    {/*Navbar*/}
     <Navbar />
-  
+    {/*Hero Section*/}
     <section className="flex min-h-screen bg-rose-100">
         {/* Left Section */}
         <div className="pl-8 w-1/2 text-taupe-800 flex items-center">
@@ -36,64 +37,125 @@ function App() {
           />
         </div>
       </section>
-      <section className="bg-amber-100 py-20">
-  <div className="max-w-6xl mx-auto -mt-40 relative z-10">
+    {/*About Me Section*/}
+      <section className="bg-amber-100 py-24">
+  <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-16 px-8">
 
-    <div className="grid md:grid-cols-3 gap-10">
+    {/* Left Side */}
+    <div className="md:w-1/2 flex justify-center">
+      <img
+        src={about}
+        alt="About Me"
+        className="w-[420px] rounded-2xl"
+      />
+    </div>
 
-      <div className="h-80 flex flex-col items-center justify-center bg-amber-50 rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
-      
-        <h3 className="text-taupe-800 font-bold text-6xl mb-3">8+</h3><br></br>
-        <p className="text-zinc-900 text-xl">
-          Projects Completed
-        </p>
+    {/* Right Side */}
+    <div className="md:w-1/2 flex flex-col">
 
-        
-      </div>
+      <h1 className="text-5xl md:text-6xl font-bold text-taupe-800 mb-8">
+        ABOUT ME
+      </h1>
 
-      <div className="h-80 flex flex-col justify-center items-center bg-amber-50 rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
-        <h3 className="text-taupe-800 font-bold text-6xl mb-3">6+</h3><br></br>
-        <p className="text-zinc-900 text-xl">
-          Extracurricular Activities
-        </p>
-      </div>
+      <p className="text-lg leading-8 text-gray-700">
+        Hello! I'm <span className="font-semibold">Jarifa</span>, a Computer
+        Science and Engineering student who enjoys turning ideas into practical
+        digital solutions. Whether it's developing a web application, building a
+        mobile app, analyzing data, or learning a new technology, I love
+        challenging myself and continuously improving my skills.
+        <br />
+       
+        Beyond academics, I enjoy leading teams, participating in competitions,
+        and organizing events that help me grow as both a developer and a
+        communicator. My goal is to build technology that is not only
+        functional, but also creates a positive impact for its users.
+      </p>
 
-      <div className="h-80 flex flex-col justify-center items-center bg-amber-50 rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
-        <h3 className="text-taupe-800 font-bold text-6xl mb-3">2</h3><br></br>
-        <p className="text-zinc-900 text-xl">
-          Training & Certifications
-        </p>
-      </div>
+      <div className="grid md:grid-cols-3 gap-10 mt-10">
+
+<div className=" flex flex-col items-center justify-center  bg-white rounded-xl p-10 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+
+  <h3 className="text-taupe-800 font-bold text-4xl mb-3">8+</h3>
+  <p className="text-zinc-900 text-md text-center">
+    Projects Completed
+  </p>
+
+  
+</div>
+
+<div className="flex flex-col justify-center items-center  bg-white rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+  <h3 className="text-taupe-800 font-bold text-4xl mb-3">6+</h3>
+  <p className="text-zinc-900 text-md text-center">
+    Extracurricular Activities
+  </p>
+</div>
+
+<div className="flex flex-col justify-center items-center bg-white rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+  <h3 className="text-taupe-800 font-bold text-4xl mb-3">2</h3>
+  <p className="text-zinc-900 text-md align-middle text-center">
+    Training & Certifications
+  </p>
+</div>
+
+</div>
 
     </div>
+
   </div>
+
 </section>
-<section className="bg-rose-100 py-20">
-  <div className="mx-auto flex md:flex-row">
-    <div className="w-1/2 flex justify-start">
-      <img src={about} alt="About" className="h-auto" />
+{/*Skills Section*/}
+  <section className="bg-amber-100 py-20">
+  <h2 className="text-5xl md:text-6xl font-bold text-taupe-800 mb-8 text-center">MY SKILLS</h2>
 
-    </div>
-    <div className="w-1/2 flex justify-end pr-60">
-      <h1 className="  text-6xl font-bold text-taupe-800">ABOUT ME</h1>
-    </div>
+  <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center gap-16 px-8">
+    
 
-  </div>
+{/* Right Side */}
+<div className="flex flex-col items-center justify-center">
+
+
+
+
+  <div className="grid md:grid-cols-3 gap-10 mt-10">
+
+<div className=" flex flex-col items-center justify-center  bg-white rounded-xl p-10 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+
+<h3 className="text-taupe-800 font-bold text-4xl mb-3">8+</h3>
+<p className="text-zinc-900 text-md text-center">
+Projects Completed
+</p>
+
+
+</div>
+
+<div className="flex flex-col justify-center items-center  bg-white rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+<h3 className="text-taupe-800 font-bold text-4xl mb-3">6+</h3>
+<p className="text-zinc-900 text-md text-center">
+Extracurricular Activities
+</p>
+</div>
+
+<div className="flex flex-col justify-center items-center bg-white rounded-xl p-6 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-y-2">
+<h3 className="text-taupe-800 font-bold text-4xl mb-3">2</h3>
+<p className="text-zinc-900 text-md align-middle text-center">
+Training & Certifications
+</p>
+</div>
+
+</div>
+
+</div>
+
+</div>
+    
+
+  
   </section>
+
       
     </>
   );
 }
 
 export default App;
-
-<div className="flex">
-      <div className="px-16 w-1/2 flex flex-col justify-center items-end bg-red-200">
-      <h1 className="text-5xl font-bold">My name is Jarifa Tasnim</h1>
-      <p className="text-lg">I am an undergraduate student.</p>
-    </div>
-    <div className="px-16 w-1/2">
-      <img className="w-full h-full object-cover" src={profile} alt="Profile" />
-    </div>
-
-    </div>
